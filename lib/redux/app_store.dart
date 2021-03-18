e6b2543c7b952ab3b0c46730147b4f28958e2600
Mateus Araujo <mateus_1542@hotmail.com>
2021-03-18 16:02:55 -0700

    feat(state-managment): examples with redux and bloc
    Author:    Mateus Araujo <mateus_1542@hotmail.com>

---
+++ lib/redux/app_store.dart
@@ -0,0 +1,36 @@
+import 'package:state_flux_bloc/redux/store.dart';
+
+enum AppAction { increment, decrement }
+
+class AppState {
+  final int value;
+
+  AppState({this.value = 0});
+
+  operator +(int newValue) {
+    return AppState(value: value + newValue);
+  }
+
+  operator -(int newValue) {
+    return AppState(value: value + newValue);
+  }
+
+  @override
+  String toString() {
+    return '$value';
+  }
+}
+
+AppState _reducer(AppState state, AppAction action) {
+  if (action == AppAction.increment) {
+    // return AppState(value: state.value + 1);
+    return state + 1;
+  } else {
+    return state - 1;
+  }
+}
+
+final appStore = Store<AppAction, AppState>(
+  initialState: AppState(),
+  reducer: _reducer,
+); // Store
